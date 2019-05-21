cb8e49ca3a8c9d6438208a79fc70bde0629111af
edgon <edgon85@gmail.com>
2019-05-21 13:16:08 -0600

    Widget personalizado - CardSwiper

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # peliculas
 
 A new Flutter application Películas
+
+[1 - Swipe de tarjetas](../../tree/046756a4fe37e2d02f7d48ed7827bd06d9ef9b30)
+
--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
         centerTitle: false,
         backgroundColor: Colors.indigoAccent,
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.search), onPressed: (){})
+          IconButton(icon: Icon(Icons.search), onPressed: () {})
         ],
       ),
       body: Container(
@@ -25,20 +25,8 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swipeCards() {
-    return Container(
-      padding: EdgeInsets.only(top: 10.0),
-      width: double.infinity,
-      height: 300.0,
-      child: Swiper(
-        itemBuilder: (BuildContext context,int index){
-          return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
-        },
-        itemCount: 3,
-        itemWidth: 200.0,
-        layout: SwiperLayout.STACK,
-        //pagination: new SwiperPagination(),
-        //control: new SwiperControl(),
-      ),
+    return CardSwiper(
+      peliculas: [1, 2, 3, 4, 5],
     );
   }
 }
