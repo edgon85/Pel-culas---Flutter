1c60728a9d247c81005b809706092f2db5a46bb7
edgon <edgon85@gmail.com>
2019-05-21 12:25:02 -0600

    Inicio del proyecto - Peliculas

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Películas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {
+        '/': (BuildContext context) => HomePage(),
+      },
+    );
+  }
+}
+
