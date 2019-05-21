046756a4fe37e2d02f7d48ed7827bd06d9ef9b30
edgon <edgon85@gmail.com>
2019-05-21 12:43:04 -0600

    Swipe de tarjetas

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Hola mundo')),
+      appBar: AppBar(
+        title: Text('Pelícuals en cines'),
+        centerTitle: false,
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: (){})
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swipeCards(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _swipeCards() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context,int index){
+          return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
+        },
+        itemCount: 3,
+        itemWidth: 200.0,
+        layout: SwiperLayout.STACK,
+        //pagination: new SwiperPagination(),
+        //control: new SwiperControl(),
+      ),
     );
   }
 }
